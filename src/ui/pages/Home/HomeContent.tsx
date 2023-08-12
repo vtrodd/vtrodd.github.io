e5f31801a308b3a6887bc2290dc9a2dcc71fa623
Vic Trodd <vic.trodd@gmail.com>
2023-08-12 17:44:27 +0100

    added tabindex to link in homecontent

--- src/ui/pages/Home/HomeContent.tsx
+++ src/ui/pages/Home/HomeContent.tsx
@@ -1,11 +1,16 @@
+import {useLocation} from 'react-router-dom'
 import './HomeContent.scss'
 
 export const HomeContent = () => {
+  const {pathname} = useLocation()
+  const path = pathname.substring(1)
+  const active = path === ''
+  
   return (
     <div>
       <h2>I'm still working on my site!</h2>
       <h3>Check back soon for more content!</h3>
-      <h4><i>In the meantime, check out <a href='https://will.trodd.dev'>my brother's site!</a></i></h4>
+      <h4><i>In the meantime, check out <a tabIndex={active ? 0 : -1} href='https://will.trodd.dev'>my brother's site!</a></i></h4>
     </div>
   )
 }
