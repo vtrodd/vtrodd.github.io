4f0d0e775eb66773f4a579f2ae9673721152d5a4
Vic Trodd <vic.trodd@gmail.com>
2023-10-31 23:02:27 +0100

    Updated /now

--- src/ui/pages/Now/NowHighlight.tsx
+++ src/ui/pages/Now/NowHighlight.tsx
@@ -1,41 +1,25 @@
 import {useLocation} from 'react-router-dom'
 import './NowHighlight.scss'
-import grad from './grad.jpg'
+import {Nows, getDateString} from './Nows'
 
 export const NowHighlight = () => {
   const {pathname} = useLocation()
   const path = pathname.substring(1)
   const active = path === 'now'
+
+  const now = Nows[0]
   
   return (
     <>
       <div className='left-highlight text-highlight'>
         <h1><a tabIndex={active ? 0 : -1} aria-hidden={!active} href='https://nownownow.com'>/now</a></h1>
-        <h3 id='update-date'><i>Updated 6th August 2023</i></h3>
+        <h3 id='update-date'><i>Updated {getDateString(now.date)}</i></h3>
         <div id='update'>
-          <h4>/life</h4>
-          <ul>
-            <li><i>I graduated from the University of York</i></li>
-            <li><i>I am applying for many jobs</i></li>
-            <li><i>Though I have also applied for a master's...</i></li>
-          </ul>
-
-          <h4>/misc</h4>
-          <ul>
-            <li><i>I've been getting back into Overwatch</i></li>
-            <li><i>I'm going to the BBC Proms soon!</i></li>
-          </ul>
-
-          <h4>/books</h4>
-          <ul>
-            <li><i>The Time Traveller's Guide to Medieval England</i></li>
-            <li><i>The Old Man and The Sea</i></li>
-            <li><i>Dracula</i></li>
-          </ul>
+          {now.content}
         </div>
       </div>
       <div className='right-highlight image-highlight'>
-        <img src={grad} alt='Vic on the day of his graduation' />
+        <img src={now.image} alt={now.imageAlt} />
       </div>
     </>
   )
---
+++ src/ui/pages/Now/Nows.tsx
@@ -0,0 +1,73 @@
+import grad from './pics/2023-08-06/grad.jpg'
+import skele from './pics/2023-10-31/skele.jpg'
+
+export const Nows = [
+  {
+    date: new Date(2023, 10 - 1, 31),
+    content: <>
+    <h4>/life</h4>
+          <ul>
+            <li><i>I graduated from the University of York</i></li>
+            <li><i>I officially start my new job at CERN tomorrow!</i></li>
+            <li><i>I haven't found somewhere to live yet so I'm staying in a hostel</i> 😭</li>
+          </ul>
+
+          <h4>/misc</h4>
+          <ul>
+            <li><i>I went, saw, and enjoyed the Lord of the Rings musical</i></li>
+            <li><i>I buried white chocolate skeletons in chocolate mousses (mice?)</i></li>
+          </ul>
+
+          <h4>/books</h4>
+          <ul>
+            <li><i>Eight Detectives (and struggling through it...)</i></li>
+            <li><i>The Time Traveller's Guide to Medieval England (still)</i></li>
+          </ul>
+    </>,
+    image: skele,
+    imageAlt: 'White chocolate skeleton being unearthed from a chocolate mousse'
+  },
+
+  {
+    date: new Date(2023, 8 - 1, 6),
+    content: <>
+      <h4>/life</h4>
+      <ul>
+        <li><i>I graduated from the University of York</i></li>
+        <li><i>I am applying for many jobs</i></li>
+        <li><i>Though I have also applied for a master's...</i></li>
+      </ul>
+
+      <h4>/misc</h4>
+      <ul>
+        <li><i>I've been getting back into Overwatch</i></li>
+        <li><i>I'm going to the BBC Proms soon!</i></li>
+      </ul>
+
+      <h4>/books</h4>
+      <ul>
+        <li><i>The Time Traveller's Guide to Medieval England</i></li>
+        <li><i>The Old Man and The Sea</i></li>
+        <li><i>Dracula</i></li>
+      </ul>
+    </>,
+    image: grad,
+    imageAlt: 'Vic on the day of his graduation'
+  }
+]
+
+export const getDateString = (date: Date) => {
+  const day = date.getDate()
+  const month = date.toLocaleString('en-GB', {month: 'long'})
+  const year = date.getFullYear()
+
+  // get ordinal
+  let ordinal = (
+    (day > 3 && day < 21) ? 'th' :
+      (day % 10 === 1) ? 'st' :
+        (day % 10 === 2) ? 'nd' :
+          (day % 10 === 3) ? 'rd' : 'th'
+  )
+
+  return `${day}${ordinal} ${month} ${year}`
+}
